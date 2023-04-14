05709468d5072ed3fe99e5455b4c35ab1ac6600f
Acris <nicolas.velez99@hotmail.com>
2023-04-14 08:23:24 -0500

    [feat]: nuevas rutas

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DocumentController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DiskController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\TransactionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,16 +24,25 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::controller(DocumentController::class)->group(function () {
-    Route::get('/documento', 'index');
-    Route::get('/documento/{id}', 'show');
-    Route::get('/documento/usuario/{id}/', 'showByUserId');
+    Route::get('/document', 'index');
+    Route::get('/document/{id}', 'show');
+    Route::get('/document/usuario/{id}/', 'showByUserId');
 });
 
 Route::controller(UserController::class)->group(function () {
-    Route::get('/usuario', 'index');
-    Route::get('/usuario/{id}', 'show');
-    Route::get('/usuario/{id}/detalles/{state?}', 'showDetails')->whereIn('state',['Firmado', 'Pendiente', 'Devuelto']);
+    Route::get('/user', 'index');
+    Route::get('/user/{id}', 'show');
+    Route::get('/user/{id}/detalles/{state?}', 'showDetails')->whereIn('state',['Firmado', 'Pendiente', 'Devuelto']);
 });
+
+Route::controller(TransactionController::class)->group(function () {
+    Route::get('/transactions', 'index');
+});
+
+Route::controller(ProductController::class)->group(function () {
+    Route::get('/product', 'index');
+});
+
 
 Route::get('/sended/document/{id}', [DiskController::class ,'downloadObject']);
 // Route::controller(DiskController::class)->group(function(){
